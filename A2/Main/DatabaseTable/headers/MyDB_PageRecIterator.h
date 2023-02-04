4e6b77d9cb6f9795dc663d9a8df1e01cb88169ae
Ming-Hsuan <ted0104@icloud.com>
2023-02-04 14:40:15 -0600

    PagRecIter v1

--- A2/Main/DatabaseTable/headers/MyDB_PageRecIterator.h
+++ A2/Main/DatabaseTable/headers/MyDB_PageRecIterator.h
@@ -13,13 +13,16 @@
 class MyDB_PageRecIterator : public MyDB_RecordIterator {
 
 public:
-    MyDB_PageRecIterator(MyDB_RecordPtr myRecord, MyDB_PageReaderWriter* myPageReaderWriter);
+    MyDB_PageRecIterator(MyDB_RecordPtr rp, MyDB_PageHandle ph);
 
     void getNext() override;
 
     bool hasNext() override;
 private:
 
+    MyDB_RecordPtr recordPtr;
+    MyDB_PageHandle pageHandle;
+    unsigned bytesUsed;
 
 };
 
--- A2/Main/DatabaseTable/source/MyDB_PageRecIterator.cc
+++ A2/Main/DatabaseTable/source/MyDB_PageRecIterator.cc
@@ -7,14 +7,29 @@
 
 #include "MyDB_PageRecIterator.h"
 
-MyDB_PageRecIterator::MyDB_PageRecIterator(MyDB_RecordPtr myRecord, MyDB_PageReaderWriter *myPageReaderWriter) {
-
+MyDB_PageRecIterator::MyDB_PageRecIterator(MyDB_RecordPtr rp, MyDB_PageHandle ph) {
+    this->recordPtr = rp;
+    this->pageHandle = ph;
+    this->bytesUsed = sizeof (PageOverlay);
 }
-void MyDB_PageRecIterator::getNext() {
 
+void MyDB_PageRecIterator::getNext() {
+    if (this->hasNext()) {
+        /*
+         * In fresh start, aka (this->bytesUsed == sizeof (PageOverlay)),
+         * pos is actually: (PageOverlay*) this->pageHandle->getBytes()->bytes
+         * */
+        void* pos = this->bytesUsed + (char*) this->pageHandle->getBytes();
+        void* newPos = this->recordPtr->fromBinary(pos);
+        this->bytesUsed += (char*) newPos - (char*) pos;
+    }
 }
 
 bool MyDB_PageRecIterator::hasNext() {
+    auto* pageOverlay = (PageOverlay*) this->pageHandle->getBytes();
+    if (pageOverlay->offsetToNextUnwritten > this->bytesUsed) {
+        return true;
+    }
     return false;
 }
 
