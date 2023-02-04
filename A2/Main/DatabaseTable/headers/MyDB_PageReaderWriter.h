c426662dab47479d22004ce56223bb875248287f
Johnson <hungjiewu@gmail.com>
2023-02-04 11:26:08 -0600

    feat: init & update PageRW

--- A2/Main/DatabaseTable/headers/MyDB_PageReaderWriter.h
+++ A2/Main/DatabaseTable/headers/MyDB_PageReaderWriter.h
@@ -5,17 +5,25 @@
 #include "MyDB_PageType.h"
 #include "MyDB_TableReaderWriter.h"
 
+struct PageOverlay{
+    unsigned offsetToNextUnwritten;
+    MyDB_PageType pageType;
+
+    char bytes[0]; // this is where to store the actual data
+};
+
 class MyDB_PageReaderWriter {
 
 public:
 
 	// ANY OTHER METHODS YOU WANT HERE
+    MyDB_PageReaderWriter(MyDB_BufferManagerPtr bufferManager, MyDB_TablePtr table, long i);
 
 	// empties out the contents of this page, so that it has no records in it
 	// the type of the page is set to MyDB_PageType :: RegularPage
 	void clear ();	
 
-	// return an itrator over this page... each time returnVal->next () is
+	// return an iterator over this page... each time returnVal->next () is
 	// called, the resulting record will be placed into the record pointed to
 	// by iterateIntoMe
 	MyDB_RecordIteratorPtr getIterator (MyDB_RecordPtr iterateIntoMe);
@@ -24,7 +32,7 @@ public:
 	// there is not enough space on the page; otherwise, return true
 	bool append (MyDB_RecordPtr appendMe);
 
-	// gets the type of this page... this is just a value from an ennumeration
+	// gets the type of this page... this is just a value from an enumeration
 	// that is stored within the page
 	MyDB_PageType getType ();
 
@@ -32,8 +40,14 @@ public:
 	void setType (MyDB_PageType toMe);
 	
 private:
+    // Use this to obtain correct memory to read record, or write record
+    PageOverlay* page;
 
-	// ANYTHING ELSE YOU WANT HERE
+    MyDB_PageHandle pageHandle;
+
+    // Check if there is enough space to write record
+    // Often used with recordPtr -> getBinarySize ()
+    size_t pageSize;
 };
 
 #endif
--- A2/Main/DatabaseTable/source/MyDB_PageReaderWriter.cc
+++ A2/Main/DatabaseTable/source/MyDB_PageReaderWriter.cc
@@ -7,19 +7,45 @@
 void MyDB_PageReaderWriter :: clear () {
 }
 
-MyDB_PageType MyDB_PageReaderWriter :: getType () {
-	return MyDB_PageType :: RegularPage;
-}
 
 MyDB_RecordIteratorPtr MyDB_PageReaderWriter :: getIterator (MyDB_RecordPtr) {
 	return nullptr;
 }
 
-void MyDB_PageReaderWriter :: setType (MyDB_PageType) {
+void MyDB_PageReaderWriter :: setType (MyDB_PageType pageType) {
+    this->page->pageType = pageType;
 }
 
-bool MyDB_PageReaderWriter :: append (MyDB_RecordPtr) {
-	return true;
+MyDB_PageType MyDB_PageReaderWriter :: getType () {
+    return this->page->pageType;
+}
+
+bool MyDB_PageReaderWriter :: append (MyDB_RecordPtr recordPtr) {
+    size_t recordSize = recordPtr->getBinarySize();
+    if (this->page->offsetToNextUnwritten + recordSize > this->pageSize) {
+        return false;
+    }
+
+    // Get the current position of the page bytes
+    auto pos = this->page->bytes[this->page->offsetToNextUnwritten];
+
+    // Write the record to the page
+    recordPtr->toBinary(&(this->page[pos]));
+
+    // Update the offset
+    this->page->offsetToNextUnwritten += recordSize;
+
+
+    return true;
+}
+
+MyDB_PageReaderWriter::MyDB_PageReaderWriter(MyDB_BufferManagerPtr bufferManager, MyDB_TablePtr tablePtr, long i) {
+    this->pageHandle = bufferManager->getPage(tablePtr, i);
+
+    page = (PageOverlay*) this->pageHandle->getBytes();
+    page->offsetToNextUnwritten = 0;
+
+    this->pageSize = bufferManager->getPageSize();
 }
 
 #endif
