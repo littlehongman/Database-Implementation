e221e3e8a59bb4e3e35cd5d29ccb5c722a4b4acd
Johnson <hungjiewu@gmail.com>
2023-02-06 14:39:09 -0600

    feat: init MyDB_TableRecIterator

--- A2/Main/DatabaseTable/headers/MyDB_TableRecIterator.h
+++ A2/Main/DatabaseTable/headers/MyDB_TableRecIterator.h
@@ -5,4 +5,25 @@
 #ifndef A1_MYDB_TABLERECITERATOR_H
 #define A1_MYDB_TABLERECITERATOR_H
 
+
+#include "MyDB_RecordIterator.h"
+#include "MyDB_PageRecIterator.h"
+
+class MyDB_TableRecIterator : public MyDB_RecordIterator {
+
+public:
+    MyDB_TableRecIterator();
+
+    void getNext() override;
+
+    bool hasNext() override;
+private:
+    MyDB_PageRecIterator pageRecIterator;
+    MyDB_TableReaderWriter *tableRW;
+    long pageId;
+
+};
+
+
+
 #endif
--- A2/Main/DatabaseTable/source/MyDB_TableRecIterator.cc
+++ A2/Main/DatabaseTable/source/MyDB_TableRecIterator.cc
@@ -9,3 +9,31 @@
 
 
 #endif
+
+void MyDB_TableRecIterator::getNext() {
+
+}
+
+bool MyDB_TableRecIterator::hasNext() {
+    // Current pageRecIterator has next
+    if (this->pageRecIterator.hasNext()) {
+        return true;
+    }
+
+    else{
+        // Current pageRecIterator does not have next
+        // Check if there is next page
+        if (this->pageId < this->tableRW->lastPage()) {
+            // There is next page
+            // Get next page
+            this->pageId++;
+            this->pageRecIterator = MyDB_PageRecIterator();
+
+            return true;
+        }
+        else{
+            // There is no next page
+            return false;
+        }
+    }
+}
