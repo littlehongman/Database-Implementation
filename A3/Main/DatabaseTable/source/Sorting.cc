92e9c539d32b0df037d2efdd0d3fd63c0450a1b2
Johnson <hungjiewu@gmail.com>
2023-02-11 17:15:36 -0600

    feat: sort main function

--- A3/Main/DatabaseTable/source/Sorting.cc
+++ A3/Main/DatabaseTable/source/Sorting.cc
@@ -7,15 +7,23 @@
 #include "MyDB_TableRecIteratorAlt.h"
 #include "MyDB_TableReaderWriter.h"
 #include "Sorting.h"
+#include "MyDB_PageListIteratorAlt.h"
 
 using namespace std;
 
-void mergeIntoFile(MyDB_TableReaderWriter &, vector<MyDB_RecordIteratorAltPtr> &, function<bool()>, MyDB_RecordPtr,
-                   MyDB_RecordPtr) {
+
+
+void mergeIntoFile(MyDB_TableReaderWriter &sortIntoMe, vector<MyDB_RecordIteratorAltPtr> &mergeUs, function<bool()> comparator, MyDB_RecordPtr lhs,
+                   MyDB_RecordPtr rhs) {
+
+//    // Define data structures
+//    priority_queue<MyDB_RecordIteratorAltPtr, vector<MyDB_RecordIteratorAltPtr>, comparator>;
+
 }
 
 
 // Literally "Merge Two Sorted Lists" from LeetCode, but merges records into a vector of pages
+// Merge two sorted pages into list of pages
 vector<MyDB_PageReaderWriter>
 mergeIntoList(MyDB_BufferManagerPtr parent, MyDB_RecordIteratorAltPtr leftIter,
               MyDB_RecordIteratorAltPtr rightIter, function<bool()> comparator, MyDB_RecordPtr lhs,
@@ -31,6 +39,8 @@ mergeIntoList(MyDB_BufferManagerPtr parent, MyDB_RecordIteratorAltPtr leftIter,
         MyDB_RecordPtr next;
         leftIter->getCurrent(lhs);
         rightIter->getCurrent(rhs);
+
+        // TODO: True => Larger or Smaller?
         if (comparator()) {
             next = lhs;
             left_has_next = leftIter->advance();
@@ -67,7 +77,55 @@ void appendToPage(const MyDB_BufferManagerPtr& parent, vector<MyDB_PageReaderWri
 }
 
 
-void sort(int, MyDB_TableReaderWriter &, MyDB_TableReaderWriter &, function<bool()>, MyDB_RecordPtr, MyDB_RecordPtr) {
+void sort(int runSize, MyDB_TableReaderWriter &sortMe, MyDB_TableReaderWriter &sortIntoMe, function<bool()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
+    // 1. The sort phase -> Get sorted runs
+
+    // (0) Define some variables
+    int page_num = sortMe.getTable()->lastPage();
+    int curr_idx = 0;
+    MyDB_BufferManagerPtr bufferMgrPtr = sortMe.getBufferMgr();
+
+    vector<MyDB_RecordIteratorAltPtr> all_pages; // store
+    queue<MyDB_RecordIteratorAltPtr> run_pages; // store pages of current run
+
+    // (1)  [1] Load a run of pages into RAM
+    //      [2] Sort each individual page
+    // Note: Since all maximum page size is identical, no need to consider memory size
+    while (curr_idx <= page_num){
+
+        // 1. Get runSize of pages
+        for (int i = 0; i < runSize && curr_idx <= page_num; i++){ // TWO conditions
+            MyDB_PageReaderWriter curr_page = sortIntoMe[curr_idx];
+
+            // TODO: sort in-place or not?
+            curr_page.getBytes(); // Load into RAM
+            curr_page.sortInPlace(comparator, lhs, rhs); // Sort individual page
+
+            run_pages.push(curr_page.getIteratorAlt()); // Push iterator into queue
+
+            curr_idx ++; // increment curr_idx
+        }
+        // TODO: what if the whole table only have one page => no need to deal;
+        // 2. Repeating called mergeToList (merge 2 sorted list), until only one big list of pages
+        while (run_pages.size() > 1){
+            MyDB_RecordIteratorAltPtr left_iter = run_pages.front();
+            run_pages.pop();
+
+            MyDB_RecordIteratorAltPtr right_iter = run_pages.front();
+            run_pages.pop();
+
+            vector<MyDB_PageReaderWriter> result = mergeIntoList(bufferMgrPtr, left_iter, right_iter, comparator, lhs, rhs);
+
+            run_pages.push(make_shared <MyDB_PageListIteratorAlt> (result));
+
+        }
+
+        all_pages.push_back(run_pages.back());
+        run_pages.pop();
+    }
+
+    // 2. The merge phase -> Merge all sorted runs
+    mergeIntoFile(sortIntoMe, all_pages, comparator, lhs, rhs);
 
 }
 
